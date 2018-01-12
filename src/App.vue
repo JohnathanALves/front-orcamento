accdf10025cfbfa5b6964e996dfb33d2f6ff9f36
Administrator@PLANTEBEM.COM <Administrator@PLANTEBEM.COM>
2018-01-12 10:31:05 -0300

    arquivos front

---
+++ src/App.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="corpo">
+    <router-view></router-view>
+  </div>
+</template>
+
+<script>
+import { routes } from './routes';
+
+export default {
+  data() {
+    return {
+      routes
+    }
+  }  
+}
+</script>
+
+<style scoped>
+
+</style>
